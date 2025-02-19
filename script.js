17abfdaad833f13f208ae1aa6506c438b66e28f5
GuidMoreira <161589016+GuidMoreira@users.noreply.github.com>
2025-02-19 11:16:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let numMes = prompt("Digite o número do mês do seu aniversário")
+let mes =["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" ]
+
+console.log ("Você nasceu no mês de "+ mes [numMes -1]);
